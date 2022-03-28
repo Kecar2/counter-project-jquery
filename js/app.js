76183bb31ac76ba55c89b78a947a3ad29526ade8
Kecar2 <kendrycarvajalsegura>
2022-03-28 10:22:23 +0200

    done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,39 @@
 let counter = 0;
 
+
 $(document).ready(function () {
     console.log("DOM cargado! Implementa tu solución a partir de esta línea.");
 
+    $(document).ready(function () {
+        console.log("DOM cargado! Implementa tu solución a partir de esta línea.");
+    
+    
+        $('.prevBtn').click(function () {
+            counter--;
+            updateDOM();
+        });
+    
+        $('.nextBtn').click(function () {
+            counter++;
+            updateDOM();
+        });
+    
+        function updateDOM() {
+            $('#counter').text(counter);
+    
+            if (counter == 0) {
+                $('#counter').removeClass('positivo');
+                $('#counter').removeClass('negativo');
+            }
+    
+            else if (counter > 0) {
+                $('#counter').addClass('positivo');
+            }
+    
+            else {
+                $('#counter').addClass('negativo');
+            }
+        }
+    });
 });
+
